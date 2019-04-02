471b2ed293ae9c500edfce205d7b65519c2a6937
Mychal Hall <hallmychal@gmail.com>
2019-04-02 14:54:07 -0500

    Friend list acquired

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/friends`)
+    .then(res => {
+      const friends = res.data;
+      this.setState({ friends })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ul>
+        {this.state.friends.map(friend => <li>{friend.name}</li>)}
+      </ul>
     );
   }
 }
