3af36f4eb163f665984020c5fca3ec82c85fdc8b
Mychal Hall <hallmychal@gmail.com>
2019-04-02 17:37:39 -0500

    Day1 MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  state = {
-    friends: []
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
   }
 
   componentDidMount() {
-    axios.get(`http://localhost:5000/friends`)
-    .then(res => {
+    axios.get(`http://localhost:5000/friends`).then(res => {
       const friends = res.data;
-      this.setState({ friends })
-    })
+      this.setState({ friends });
+    });
   }
 
   render() {
     return (
       <ul>
-        {this.state.friends.map(friend => <li>{friend.name}</li>)}
+        {this.state.friends.map(friend => (
+          <li>{friend.name}</li>
+        ))}
       </ul>
     );
   }
