908c9bfd5c7c3a5a3a9b10160c4f082cacab6ae2
Mychal Hall <hallmychal@gmail.com>
2019-04-04 14:17:59 -0500

    Completed Router set up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 import Friends from "./components/friends";
 
 class App extends Component {
@@ -11,6 +13,10 @@ class App extends Component {
     };
   }
 
+  updateFriends = newFriends => {
+    this.setState({ friends: newFriends });
+  }
+
   componentDidMount() {
     axios.get(`http://localhost:5000/friends`).then(res => {
       const friends = res.data;
@@ -19,8 +25,11 @@ class App extends Component {
   }
 
   render() {
-    return <Friends friends={this.state.friends} />;
-  }
+    return( 
+    <div className='App'>
+    <Route exact path='/' render={props=> (<Friends {...props} friends={this.state.friends} />)}/>
+    </div>
+    )}
 }
 
 export default App;
